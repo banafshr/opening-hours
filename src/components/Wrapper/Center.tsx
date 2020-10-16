09dab10a23a5ee82b41d2ee66f90841ca11117ac
Bani <banafsheh.rahnama@eficode.com>
2020-10-16 17:11:59 +0300

    rename folders

--- src/components/Wrapper/Center.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import css from './Wrapper.module.css';
-
-const Center: FC = ({ children }) => {
-  return <div className={css.center}>{children}</div>;
-};
-
-export default Center;
